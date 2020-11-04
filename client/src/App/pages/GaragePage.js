c1e6311e25e1cbca6f4d0c9148638430244c8e7e
Edmond Fang <edmond.fang@berkeley.edu>
2020-11-04 00:40:42 -0800

    garage chart

--- client/src/App/pages/GaragePage.js
+++ client/src/App/pages/GaragePage.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSync } from '@fortawesome/free-solid-svg-icons'
+import { VictoryBar, VictoryChart, VictoryHistogram, VictoryAxis } from 'victory';
 
-class GarageControlPage extends Component {
+class GaragePage extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -11,11 +12,23 @@ class GarageControlPage extends Component {
       isOpen: false,
       loadingGarageState: true,
       loadingData: true,
-      data: []
+      data: [],
+      currTime: new Date()
     }
 
     this.refreshGarageState = this.refreshGarageState.bind(this)
     this.handleRefresh = this.handleRefresh.bind(this)
+    this.getGarageData = this.getGarageData.bind(this)
+  }
+
+  getGarageData = async function () {
+    const response = await fetch('/api/getdata/all_garage_data')
+    let rawData = await (response.json())
+    rawData.sort(o => o.time)
+    // rawData = rawData.map(k => { return { x: -(this.state.currTime.getTime() - k.time) / 3.6e+6 } })
+    rawData = rawData.map(k => { return { x:  new Date(k.time)}  })
+    this.setState({ data: rawData })
+    console.log(rawData)
   }
 
   refreshGarageState = async function () {
@@ -38,6 +51,7 @@ class GarageControlPage extends Component {
   componentDidMount() {
     document.title = 'Garage'
     this.refreshGarageState()
+    this.getGarageData()
   }
   render() {
     return (
@@ -53,20 +67,39 @@ class GarageControlPage extends Component {
                       <span className="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>
                       : <FontAwesomeIcon icon={faSync} className="mr-2" />}
                     Refresh
-                  </button>                
-                     <div>
-                      <h2 className={this.state.loadingGarageState ? "text-white" : ""}>Garage is {this.state.currentGarageState},</h2>
-                      <p className="text-muted">and has been since {this.state.timeOfLastCycle}.</p>
-                      <p className="text-muted">By the way, today is {(new Date()).toDateString()}.</p>
-                    </div>
-                  
+                  </button>
+                  <div>
+                    <h2 className={this.state.loadingGarageState ? "text-white" : ""}>Garage is {this.state.currentGarageState},</h2>
+                    <p className="text-muted">and has been since {this.state.timeOfLastCycle}.</p>
+                    <p className="text-muted">By the way, today is {(new Date()).toDateString()}.</p>
+                  </div>
+
                 </div>
               </div>
             </div>
           </div>
           <div className="row justify-content-center">
             <div className="col-10 col-lg-8">
-              <p>Unfortunately, you cannot control the garage from this site. For security reasons. Not because it's difficult or something. Yep.</p>
+              <p>Activity in the last 12 hours:</p>
+              <div>
+                <VictoryChart
+                  domainPadding={{ x: 20 }}
+                >
+                  {/* <VictoryAxis
+                    label="Hours ago"
+                  /> */}
+                  <VictoryHistogram
+                    style={{
+                      data: { fill: "#c43a31" }
+                    }}
+                    data={this.state.data}
+
+                    // bins={[...Array(48).keys()].map(k => -12 + k / 4)}
+                    bins={[...Array(36).keys()].map(k => new Date(this.state.currTime.getTime() - 4.32e+7 + k*3.6e+6/3))}
+                  />
+
+                </VictoryChart>
+              </div>
             </div>
           </div>
         </div>
@@ -74,4 +107,4 @@ class GarageControlPage extends Component {
     );
   }
 }
-export default GarageControlPage;
+export default GaragePage;
